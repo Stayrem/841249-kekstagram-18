0289e98a5e23facf9589ec0a838e529f7c14dfa6
Stayrem <spirin2205@yandex.ru>
2019-09-24 00:21:11 +0300

    добавлена рандомная генерация коментов

--- js/main.js
+++ js/main.js
@@ -6,34 +6,43 @@ var MIN_AVATAR_INDEX = 1;
 var MAX_AVATAR_INDEX = 6;
 var NAMES = ['Артём', 'Дмитрий', 'Арина', 'Данила', 'Алексей', 'Надежда'];
 var MESSAGES = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
-
-var randomInt = function (min, max) {
+var DESCRIPTIONS = ['Далеко-далеко за словесными горами в стране гласных и согласных живут рыбные тексты. Вдали от всех живут они в буквенных домах на берегу Семантика большого языкового океана.', 'Маленький ручеек Даль журчит по всей стране и обеспечивает ее всеми необходимыми правилами. Эта парадигматическая страна.', ' в которой жаренные члены предложения залетают прямо в рот. Даже всемогущая пунктуация не имеет власти над рыбными текстами, ведущими безорфографичный образ жизни', ' Однажды одна маленькая строчка рыбного текста по имени'];
+var getRandomNumber = function (min, max) {
   var random = min + Math.random() * (max + 1 - min);
   return Math.floor(random);
 };
-
+var comments = [];
+for (var j = 0; j < getRandomNumber(1, 8); j++) {
+  var comment = {
+    avatar: '../img/avatar/' + getRandomNumber(MIN_AVATAR_INDEX, MAX_AVATAR_INDEX) + '.svg',
+    name: NAMES[getRandomNumber(0, NAMES.length - 1)],
+    message: MESSAGES[getRandomNumber(0, MESSAGES.length)]
+  };
+  comments.push(comment);
+}
 var getPictures = function () {
   var photoInfo = [];
   for (var i = 1; i <= 25; i++) {
     var picture = {
       url: 'photos/' + i + '.jpg',
-      description: '',
-      likes: randomInt(MIN_LIKES, MAX_LIKES),
-      comments: [{
-        avatar: '../img/avatar/' + randomInt(MIN_AVATAR_INDEX, MAX_AVATAR_INDEX) + '.svg',
-        name: NAMES[randomInt(0, NAMES.length - 1)],
-        message: MESSAGES[randomInt(0, MESSAGES.length)]
-      },
-      {
-        avatar: '../img/avatar/' + randomInt(MIN_AVATAR_INDEX, MAX_AVATAR_INDEX) + '.svg',
-        name: NAMES[randomInt(0, NAMES.length - 1)],
-        message: MESSAGES[randomInt(0, MESSAGES.length)]
-      },
-      {
-        avatar: '../img/avatar/' + randomInt(MIN_AVATAR_INDEX, MAX_AVATAR_INDEX) + '.svg',
-        name: NAMES[randomInt(0, NAMES.length - 1)],
-        message: MESSAGES[randomInt(0, MESSAGES.length)]
-      }
+      description: DESCRIPTIONS[getRandomNumber(0, DESCRIPTIONS.length - 1)],
+      likes: getRandomNumber(MIN_LIKES, MAX_LIKES),
+      comments: [
+        {
+          avatar: '../img/avatar/' + getRandomNumber(MIN_AVATAR_INDEX, MAX_AVATAR_INDEX) + '.svg',
+          name: NAMES[getRandomNumber(0, NAMES.length - 1)],
+          message: MESSAGES[getRandomNumber(0, MESSAGES.length)]
+        },
+        {
+          avatar: '../img/avatar/' + getRandomNumber(MIN_AVATAR_INDEX, MAX_AVATAR_INDEX) + '.svg',
+          name: NAMES[getRandomNumber(0, NAMES.length - 1)],
+          message: MESSAGES[getRandomNumber(0, MESSAGES.length)]
+        },
+        {
+          avatar: '../img/avatar/' + getRandomNumber(MIN_AVATAR_INDEX, MAX_AVATAR_INDEX) + '.svg',
+          name: NAMES[getRandomNumber(0, NAMES.length - 1)],
+          message: MESSAGES[getRandomNumber(0, MESSAGES.length)]
+        }
       ]
     };
 
@@ -44,17 +53,17 @@ var getPictures = function () {
 
 var pictures = getPictures();
 var renderPictures = function (data) {
-  var template = document.querySelector('#picture').content.querySelector('a');
-
+  var template = document.querySelector('#picture').content.querySelector('.picture');
+  var picturesContainer = document.querySelector('.pictures');
   var fragment = document.createDocumentFragment();
   for (var i = 0; i < data.length; i++) {
     var element = template.cloneNode(true);
-    element.querySelector('.picture__img').setAttribute('src', data[i]['url']);
-    element.querySelector('.picture__comments').textContent = data[i]['comments'].length;
-    element.querySelector('.picture__likes').textContent = data[i]['likes'];
+    element.querySelector('.picture__img').src = data[i].url;
+    element.querySelector('.picture__comments').textContent = data[i].comments.length;
+    element.querySelector('.picture__likes').textContent = data[i].likes;
     fragment.appendChild(element);
   }
-  document.querySelector('.pictures').appendChild(fragment);
+  picturesContainer.appendChild(fragment);
 };
 
 renderPictures(pictures);

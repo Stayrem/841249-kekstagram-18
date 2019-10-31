dc3a6cdc6a3c923ae088305d041306b466db4510
Deadite <spirin2205@yandex.ru>
2019-10-31 22:45:19 +0300

    функция отрытие фотографии

--- js/utils.js
+++ js/utils.js
@@ -22,6 +22,7 @@
         fragment.appendChild(element);
       }
       picturesContainer.appendChild(fragment);
+      window.utils.showBigPicture();
     },
     errorHandler: function () {
 
@@ -55,6 +56,53 @@
       document.addEventListener('keydown', escapePressListener);
 
       document.addEventListener('click', errorCloseEventListener);
+    },
+    showBigPicture: function () {
+      var firstPicture = document.querySelector('.picture');
+      var bigPicture = document.querySelector('.big-picture');
+      var image = bigPicture.querySelector('.big-picture__img img');
+      var likesCount = bigPicture.querySelector('.likes-count');
+      var commentsCount = bigPicture.querySelector('.comments-count');
+      var socialCaption = bigPicture.querySelector('.social__caption');
+      var comments = bigPicture.querySelector('.social__comments');
+      var socialCommentCount = bigPicture.querySelector('.social__comment-count');
+      var commentsLoader = bigPicture.querySelector('.comments-loader');
+      socialCommentCount.classList.add('visually-hidden');
+      commentsLoader.classList.add('visually-hidden');
+      var setComments = function () {
+
+
+        var commentsLength = window.globalVars.responseData[0].comments.length;
+        var fragment = document.createDocumentFragment();
+        for (var i = 0; i < commentsLength; i++) {
+          var li = document.createElement('li');
+          li.classList.add('social__comment');
+          var img = document.createElement('img');
+          img.classList.add('social__picture');
+          img.setAttribute('width', '35');
+          img.setAttribute('height', '35');
+          var p = document.createElement('p');
+          p.classList.add('social__text');
+          img.src = window.globalVars.responseData[0].comments[i].avatar;
+          img.alt = window.globalVars.responseData[0].comments[i].name;
+          p.textContent = window.globalVars.responseData[0].comments[i].message;
+          li.appendChild(img);
+          li.appendChild(p);
+          console.log(li)
+          fragment.appendChild(li);
+        }
+        comments.appendChild(fragment);
+      };
+      firstPicture.addEventListener('click', function (evt) {
+        evt.preventDefault();
+        image.src = window.globalVars.responseData[0].url;
+        likesCount.textContent = window.globalVars.responseData[0].likes;
+        commentsCount.textContent = window.globalVars.responseData[0].comments.length;
+        socialCaption.textContent = window.globalVars.responseData[0].description;
+        comments.innerHTML = '';
+        setComments();
+        bigPicture.classList.remove('hidden');
+      });
     }
   };
 })();

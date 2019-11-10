529e094c485d86c4e9103adc274d15339a06e5ca
Deadite <spirin2205@yandex.ru>
2019-11-10 23:41:51 +0300

    увеличение изображения

--- js/modal.js
+++ js/modal.js
@@ -8,9 +8,30 @@
   var imageUploadInput = imageUploadForm.querySelector('.img-upload__input');
   var imageEditCloseBtn = imageEditor.querySelector('#upload-cancel');
   var commentsInput = document.querySelector('.text__description');
+  var photoSmallerButton = document.querySelector('.scale__control--smaller');
+  var photoBiggerButton = document.querySelector('.scale__control--bigger');
+  var scaleControl = document.querySelector('.scale__control--value');
+  var scaleControlValue = 100;
+  var image = document.querySelector('.img-upload__preview img');
+
+  var enlargePicture = function () {
+    if (scaleControlValue < window.constants.MAX_PHOTO_SCALE) {
+      scaleControlValue += window.constants.MIN_PHOTO_SCALE;
+      image.style.transform = 'scale(' + scaleControlValue / 100 + ')';
+      scaleControl.value = scaleControlValue + '%';
+    }
+  };
+  var reducePicture = function () {
+    if (scaleControlValue > window.constants.MIN_PHOTO_SCALE) {
+      scaleControlValue -= window.constants.MIN_PHOTO_SCALE;
+      image.style.transform = 'scale(' + scaleControlValue / 100 + ')';
+      scaleControl.value = scaleControlValue;
+    }
+  };
 
   var closePopup = function () {
     imageEditor.classList.add('hidden');
+    image.style.transform = 'scale(1)';
     imageUploadForm.reset();
   };
 
@@ -34,6 +55,9 @@
     effectRadioFieldset.addEventListener('change', window.setEffect);
     document.addEventListener('keydown', closeImageEditorOnEsc);
     imageEditCloseBtn.addEventListener('click', closePopup);
+    photoBiggerButton.addEventListener('click', enlargePicture);
+    photoSmallerButton.addEventListener('click', reducePicture);
+
   });
 
 })();
